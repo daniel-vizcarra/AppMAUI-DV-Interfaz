aac6744783860d9e7bac4be83b9a5d330ed69772
Daniel Alberto Vizcarra Gaibor <75272896+daniel-vizcarra@users.noreply.github.com>
2024-05-26 21:16:06 -0500

    Interfaz para Proyecto MAUI

---
+++ AtticaDV/AtticaDV/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AtticaDV
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AtticaDV/AtticaDV/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace AtticaDV
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {       
+            InitializeComponent();
+        }
+
+        private void OnMenuButtonClicked(object sender, EventArgs e)
+        {
+            menuPopup.IsVisible = !menuPopup.IsVisible;
+        }
+
+    }
+
+}
